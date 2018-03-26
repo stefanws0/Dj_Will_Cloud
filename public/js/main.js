211dd8b81699e584c26562ba928222908d2d49db
Stefan Willems <sgpwille@avans.nl>
2018-03-26 13:02:53 +0200

    Edit works now with conformation modal

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,1 @@
-$('#deleteModal').on('show.bs.modal', function (event) {
-  let button = $(event.relatedTarget); // Button that triggered the modal
-  let id = button.data('id'); // Extract info from data-* attributes
-  let title = button.data('title');
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  let modal = $(this);
-  modal.find('#deleteModalTitle').text("Delete " + title + "?");
-  modal.find('#deleteModalDescription').text("Are you sure you want to delete the products " + title + "?");
-  modal.find('#deleteModalDeleteButton').attr("href", "/dashboard/products/")
 
-});
