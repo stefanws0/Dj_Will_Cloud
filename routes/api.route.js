7ed011f261cda316f7f498eac88978d8f5a4d060
Stefan Willems <sgpwille@avans.nl>
2018-04-01 17:31:38 +0200

    asd

--- routes/api.route.js
+++ routes/api.route.js
@@ -12,24 +12,12 @@ const users = require('./api/users.route');
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use('/api/v1', router);
 router.use('/', authentication);
-router.use('/products', isLoggedIn, products);
-router.use('/brands', isLoggedIn, brands);
-router.use('/types', isLoggedIn, types);
-router.use('/users', isLoggedIn, isAdmin, users);
+router.use('/products', products);
+router.use('/brands', brands);
+router.use('/types', types);
+router.use('/users', isAdmin, users);
 
 module.exports = router;
-
-function isLoggedIn(req, res, next) {
-
-  // if user is authenticated in the session, carry on
-  if (req.isAuthenticated()) {
-    console.log("isauthenticated");
-    return next();
-  }
-
-  // if they aren't redirect them to the home page
-  res.redirect('/login');
-}
 
 function isAdmin(req, res, next) {
 
