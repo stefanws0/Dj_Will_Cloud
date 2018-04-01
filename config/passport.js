662e089c1170df11b53e5689977072033010c140
Stefan Willems <sgpwille@avans.nl>
2018-04-01 16:59:12 +0200

    console logs

--- config/passport.js
+++ config/passport.js
@@ -99,18 +99,17 @@ module.exports = function (passport) {
       // we are checking to see if the user trying to login already exists
       User.findOne({'local.email': email}, function (err, user) {
         // if there are any errors, return the error before anything else
+        console.log(err);
+        console.log(user);
         if (err)
-          console.log(err);
           return done(err);
 
         // if no user is found, return the message
         if (!user)
-          console.log("No user found.");
           return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
 
         // if the user is found but the password is wrong
         if (!user.validPassword(password))
-          console.log("Oops! Wrong password");
           return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
         // all is well, return successful user
